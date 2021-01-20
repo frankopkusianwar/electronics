2f01111faad2b89e5bb6e5af5590036df0ebcf74
frankopkusianwar <okirorfrank3@gmail.com>
2021-01-20 13:47:32 +0300

    Add single item file

---
+++ src/containers/item.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Item = () => {
+  return (
+    <div className="col-3">
+      <Categoryasidetop />
+      <Categoryasidemiddle />
+      <Categoryasidebottom />
+    </div>
+  );
+}
+
+export default Item;
