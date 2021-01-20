c452194badaf6e31d3989b6daa1a906317447504
frankopkusianwar <okirorfrank3@gmail.com>
2021-01-20 12:05:32 +0300

    Add category item frame

--- src/containers/CategoryList.js
+++ src/containers/CategoryList.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const CategoryList = () => {
   return(
-    <div className="container">
+    <div className="mx-5">
       <div className="row mt-3">
-        <div className="col-4">
+        <div className="col-3">
           <div className="top-aside">
             <h5 className="p-2">Categories</h5>
             <p className="px-2">Computers</p>
@@ -69,7 +69,31 @@ const CategoryList = () => {
             </form>
           </div>
         </div>
-        <div className="col-8">2</div>
+        <div className="col-9 category-content">
+          <div className="row category-item">
+            <div className="col-3">
+              <img src="" alt="category-item" />
+            </div>
+            <div className="col-9 p-2">
+              <div className="d-flex justify-content-between category-item-title">
+                <h5>HP ProBook 635 Aero G7</h5>
+                <h4>Ush 1,800,000</h4>
+              </div>
+              <p>
+                In perfect working condition with wide screen and long life battery, no technical faults, 
+                properly...  
+              </p>
+              <div className="category-item-tags d-flex">
+                <p className="px-2 py-1">tag1 text</p>
+                <p className="px-2 py-1 mx-4">tag2 text</p>
+                <p className="px-2 py-1">tag3 text</p>
+              </div>
+              <div className="category-location-text">
+                <small>location here</small>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
